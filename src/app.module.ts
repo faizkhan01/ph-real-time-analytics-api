6cb1ab264f4dee29a54b2b741448d5349396f8d8
Md. Faizur Rahman Khan <khan.nayeem8662@gmail.com>
2024-06-08 23:24:20 +0600

    feat: appConfigModule, dbConfig, userModule and resultsModule loaded in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ResultsModule } from './modules/result/results.module';
+import { serverConfig } from './config/server.config';
+import { swaggerConfig } from './config/swagger.config';
+import { AppConfigModule } from './config/config.module';
+import config from './db/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRoot(config),
+    UserModule,
+    ResultsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  onApplicationBootstrap() {
+    if (process.env.NODE_ENV === 'development') {
+      Logger.verbose(
+        `🧠 API Documentation running here 👀 http://${serverConfig.host}:${serverConfig.port}/${swaggerConfig.swaggerPrefix}`,
+      );
+    }
+  }
+}
